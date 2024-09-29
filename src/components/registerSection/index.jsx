b6b5105e3d420ea88a046d362b386d25d7cc182f
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-09-29 14:52:12 +0700

    Refactor registerSection component and styles

--- src/components/registerSection/index.jsx
+++ src/components/registerSection/index.jsx
@@ -1,49 +1,56 @@
 import React from "react";
 import "./styles.scss";
 
-const index = () => {
+const RegisterForm = ({ handleStepChange }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleStepChange(3);
+  };
+
   return (
     <div className="registration-form">
       <div className="login-prompt">
         Sudah punya akun? <a href="#">Klik di sini untuk masuk</a>
       </div>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <input type="text" placeholder="Nama Lengkap" />
+          <input type="text" placeholder="Nama Lengkap" required />
           <div className="input-group">
-            <select defaultValue="+62">
+            <select defaultValue="+62" required>
               <option value="+62">+62</option>
               {/* Add more country codes as needed */}
             </select>
-            <input type="text" placeholder="Nomor WhatsApp" />
+            <input type="text" placeholder="Nomor WhatsApp" required />
           </div>
         </div>
         <div className="form-group">
-          <select>
+          <select required>
             <option>Jenis Kelamin</option>
             <option value="male">Laki-laki</option>
             <option value="female">Perempuan</option>
           </select>
           <div className="input-group">
-            <select>
+            <select required>
               <option>Tanggal</option>
               {/* Add day options */}
             </select>
-            <select>
+            <select required>
               <option>Bulan</option>
               {/* Add month options */}
             </select>
-            <select>
+            <select required>
               <option>Tahun</option>
               {/* Add year options */}
             </select>
           </div>
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email" />
+          <input type="email" placeholder="Email" required />
           <input
             type="password"
             placeholder="Buat Password (minimal 5 karakter)"
+            required
+            minLength="5"
           />
         </div>
         <p className="note">
@@ -59,4 +66,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default RegisterForm;
