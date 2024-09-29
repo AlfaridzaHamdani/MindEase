1b187d1ec093f5e3f1841c5d5c0e6cbf326d3ac8
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-09-29 14:52:18 +0700

    Refactor appointment life-coach form component and add step functionality

--- src/pages/appointment/life-coach/Form.jsx
+++ src/pages/appointment/life-coach/Form.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
 import Package from "../../../components/packageConselingSection";
@@ -5,14 +6,28 @@ import Register from "../../../components/registerSection";
 import ProgressBar from "../../../components/progresBarSection";
 
 const Form = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const handleStepChange = (step) => {
+    setCurrentStep(step);
+  };
+
   return (
     <>
       <Navbar />
-      <ProgressBar />
-      <Package />
-      <Register />
+
+      <ProgressBar
+        currentStep={currentStep}
+        handleStepChange={handleStepChange}
+      />
+
+      {currentStep === 1 && <Package handleStepChange={handleStepChange} />}
+      {currentStep === 2 && <Register handleStepChange={handleStepChange} />}
+      {currentStep === 3 && <div>Pembayaran Section</div>}
+
       <Footer />
     </>
   );
 };
+
 export default Form;
