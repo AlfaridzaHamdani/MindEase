5fe62001de4398b004e2667eb8051cf9273b747f
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-09-29 14:52:05 +0700

    Refactor progress bar component and styles

--- src/components/progresBarSection/index.jsx
+++ src/components/progresBarSection/index.jsx
@@ -1,23 +1,46 @@
-// ProgressBar.jsx
 import React from "react";
 import "./styles.scss";
 
-const ProgressBar = () => {
+const ProgressBar = ({ currentStep, handleStepChange }) => {
   return (
     <div className="progress-bar-container">
       <h2 className="progress-title">Pendaftaran Konseling</h2>
       <div className="progress-bar">
-        <div className="progress-step active">
+        {/* Step 1 */}
+        <div
+          className={`progress-step ${currentStep >= 1 ? "active" : ""} ${
+            currentStep > 1 ? "completed" : ""
+          }`}
+          onClick={() => handleStepChange(1)}
+        >
           <div className="step-circle">1</div>
           <span className="step-label">Paket</span>
         </div>
-        <div className="progress-line"></div>
-        <div className="progress-step">
+
+        <div
+          className={`progress-line ${currentStep > 1 ? "completed-line" : ""}`}
+        ></div>
+
+        {/* Step 2 */}
+        <div
+          className={`progress-step ${currentStep >= 2 ? "active" : ""} ${
+            currentStep > 2 ? "completed" : ""
+          }`}
+          onClick={() => handleStepChange(2)}
+        >
           <div className="step-circle">2</div>
           <span className="step-label">Profil</span>
         </div>
-        <div className="progress-line"></div>
-        <div className="progress-step">
+
+        <div
+          className={`progress-line ${currentStep > 2 ? "completed-line" : ""}`}
+        ></div>
+
+        {/* Step 3 */}
+        <div
+          className={`progress-step ${currentStep >= 3 ? "active" : ""}`}
+          onClick={() => handleStepChange(3)}
+        >
           <div className="step-circle">3</div>
           <span className="step-label">Pembayaran</span>
         </div>
