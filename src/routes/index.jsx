7d8f5b6d07dbd4f16bae621c308a33f6c6afab46
AlfaridzaHamdani <alfaridzah@gmail.com>
2024-09-29 14:27:53 +0700

    Fix route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,11 +4,7 @@ import Schedule from "../pages/Schedule";
 import AboutUs from "../pages/AboutUs";
 import Appointment from "../pages/Appointment";
 import LifeCoach from "../pages/LifeCoach";
-<<<<<<< Updated upstream
 import Form from "../pages/appointment/life-coach/Form";
-=======
-import Psychology from "../pages/Psychology";
->>>>>>> Stashed changes
 
 function RoutesIndex() {
   return (
@@ -23,13 +19,8 @@ function RoutesIndex() {
       <Route path="/appointment" element={<Appointment />} />
 
       <Route path="/appointment/life-coach" element={<LifeCoach />} />
-<<<<<<< Updated upstream
       {/* route for Form.jsx */}
       <Route path="/appointment/life-coach/form" element={<Form />} />
-=======
-
-      <Route path="/appointment/psychology" element={<Psychology />} />
->>>>>>> Stashed changes
     </Routes>
   );
 }
