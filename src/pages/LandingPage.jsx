5ccc51b1ad1caac50e9624f25110bba501d4d058
AlfaridzaHamdani <alfaridzah@gmail.com>
2025-02-23 21:52:29 +0700

    Add lenis package and update import statement for Footer component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,8 @@ import Services from "../components/servicesSection";
 import Explanation from "../components/explanationSection";
 import Testimonial from "../components/testimonialSection";
 import CTA from "../components/ctaSection";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
+
 import Heading from "../components/heading";
 
 const LandingPage = () => {
