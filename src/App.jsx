9bd24a623b8d4e4ab5cc4e8b06c5787c514f1371
AlfaridzaHamdani <alfaridzah@gmail.com>
2025-02-23 21:42:41 +0700

    Fix import statements for consistency and formatting in App.jsx and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Routes from "./routes";
-import ScrollToTop from "./components/scrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 import SmoothScroll from "./components/SmoothScroll";
 
