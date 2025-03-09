e16b576c4fc5ecd0d7b4814671c0f8c0210e682d
Alfaridza Hamdani <alfaridzah@gmail.com>
2025-03-09 21:07:09 +0700

    Refactor Vite configuration by removing unused compression plugins and updating permissions-policy header

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compression from "vite-plugin-compression";
 
+// https://vitejs.dev/config/w
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({ algorithm: "gzip" }),
-    compression({ algorithm: "brotliCompress", ext: ".br" }),
-  ],
+  plugins: [react()],
   server: {
     headers: {
-      "Permissions-Policy": "interest-cohort=()",
-    },
-  },
-  build: {
-    minify: "true",
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
+      "permissions-policy": "interest-cohort=()",
     },
   },
 });
